1adfdd7ed783ceef65806e508c1ec840363ffb08
Stephen R. D'Souza <stephen.roy.dsouza@gmail.com>
2019-10-25 20:54:01 +1100

    added images

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
 import Navbar from "./components/Navbar/navbar";
 import Header from "./components/Header/header";
+import Main from "./components/main/main";
+import Characters from "./characters.json";
+import Wrapper from "./components/Wrapper";
 function App() {
   return (
     <div>
-      <Navbar/>
-      <Header/>
+      <Navbar />
+      <Header />
+      <Wrapper>
+        {Characters.map(character => (
+          <Main
+            id={character.id}
+            key={character.id}
+            name={character.name}
+            image={character.image}
+          />
+        ))}
+      </Wrapper>
     </div>
   );
 }
