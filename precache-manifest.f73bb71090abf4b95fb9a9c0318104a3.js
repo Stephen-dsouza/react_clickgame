9ea1e59fc55e41007a2a5d0a229e403f068957ee
Stephen R. D'Souza <stephen.roy.dsouza@gmail.com>
2019-11-01 19:35:04 +1100

    Updates

---
+++ precache-manifest.f73bb71090abf4b95fb9a9c0318104a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "287e20b314797770e97af2ec5d6d9158",
+    "url": "/react_clickgame/index.html"
+  },
+  {
+    "revision": "38c6cdb917dd87af871e",
+    "url": "/react_clickgame/static/css/main.ce77feb6.chunk.css"
+  },
+  {
+    "revision": "753452498318d157e616",
+    "url": "/react_clickgame/static/js/2.a355c688.chunk.js"
+  },
+  {
+    "revision": "38c6cdb917dd87af871e",
+    "url": "/react_clickgame/static/js/main.39d6652b.chunk.js"
+  },
+  {
+    "revision": "7bd3a897ddf7b98bcf53",
+    "url": "/react_clickgame/static/js/runtime-main.1d74fbf9.js"
+  }
+]);
