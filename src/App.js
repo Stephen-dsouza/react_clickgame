3ac2e7a9c566d0d96466133d5e945728a05ee743
Stephen R. D'Souza <stephen.roy.dsouza@gmail.com>
2019-11-01 19:37:04 +1100

    publish to git

--- src/App.js
+++ src/App.js
@@ -6,24 +6,27 @@ import Main from "./components/main/main";
 import Characters from "./characters.json";
 import Wrapper from "./components/Wrapper";
 class App extends React.Component {
+  //Set the state  of state,topscore,guess clicked id,message
   state = {
     score: 0,
     topScore: 0,
     guessClick: [],
-    message: "Click to start",
-    Characters
+    message: "Click to start"
   };
-
+  //Handle a correct guess on click
   handleCorrectGuess = newData => {
+    //Capture the state  for score and top score
     const { topScore, score } = this.state;
+    //Increase the score
     const newScore = score + 1;
+    //check if the score is greater than top score ans set the state
     const newTopScore = Math.max(newScore, topScore);
     this.setState({
       score: newScore,
       topScore: newTopScore
     });
   };
-
+  // Handle incorrect guess.These are id that have been previously clicked and exist in the array
   handleIncorrectGuess = newData => {
     this.setState({
       score: 0,
@@ -31,11 +34,11 @@ class App extends React.Component {
       message: "You lost,Try Again"
     });
   };
-
+  //On click fuction
   clickcard = id => {
-    console.log(id);
-
+    //capture the current state of array of all images that havebeen clicked
     let guessClick = this.state.guessClick;
+    //use the find method to find if the clicked image id exists in the array
     let guessed = guessClick.find(item => {
       return item === id;
     });
@@ -51,13 +54,6 @@ class App extends React.Component {
       this.handleCorrectGuess();
     }
 
-    // message: "Good Guess",
-    //   topScore:
-    //     this.setState.score > this.state.topScore
-    //       ? this.state.topScore
-    //       : this.topScore
-
-    // }
     console.log(this.state);
   };
 
