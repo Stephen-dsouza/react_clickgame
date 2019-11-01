be36096ff11a1604bff5fddfbfc4c0257523354d
Stephen R. D'Souza <stephen.roy.dsouza@gmail.com>
2019-11-01 22:43:49 +1100

    publish to git

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -3,20 +3,14 @@ import "./main.css";
 
 const CharacterCard = props => {
   return (
-    <div className="card">
+    <div className="card col-md-3">
       <div
         onClick={() => props.handleClick(props.id)}
         className="img-container"
       >
         <img alt={props.name} src={props.image} />
       </div>
-      <div className="content">
-        <ul>
-          <li>
-            <strong>Name:</strong> {props.name}
-          </li>
-        </ul>
-      </div>
+      <div className="content"></div>
     </div>
   );
 };
